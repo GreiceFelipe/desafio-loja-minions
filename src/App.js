9d6543e3dfd46792f7d084b3b73d0c81f8d4fcc0
EC2 Default User <ec2-user@ip-172-31-10-171.us-east-2.compute.internal>
2018-12-24 17:15:11 +0000

    Listagem com todos os minios.

--- src/App.js
+++ src/App.js
@@ -3,23 +3,80 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container-fluid p-0">
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+          
+          <a className="navbar-brand" href="#"><i className="fas fa-shopping-bag"></i> Loja dos Minions</a>
+          
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-menu"
+            aria-controls="navbar-menu" aria-expanded="false" aria-label="Toggle navagation">
+            <span className="navbar-toggler-icon"> </span>
+          </button>
+          
+          <div className="collapse navbar-collapse" id="navbar-menu">
+            <div className="navbar-nav">
+              <a className="nav-item nav-link" href="#">Home</a>
+              <a className="nav-item nav-link" href="#">Fale conosco</a>
+              <a className="nav-item nav-link" href="#">Login</a>
+            </div>
+          </div>
+          
+        </nav>
+        
+        
+        <div className="container p-3">
+          <ul className="list-unstyled">
+            <li className="media pt-3">
+              <img src="https://s3.us-east-2.amazonaws.com/minios-imagens/minio-1.png" alt="" className="mr-3 img-pag-pricipal"/>
+              <div className="media-body">
+                <h5 className="mt-0 mb-1">Boneco Minios Bob</h5>
+                <p>Boneco Bob do filme minios.</p>
+                <button type="button" className="btn btn-outline-dark">Reservar</button>
+              </div>
+            </li>
+             <li className="media pt-3">
+              <img src="https://s3.us-east-2.amazonaws.com/minios-imagens/minio-2.png" alt="" className="mr-3 img-pag-pricipal"/>
+              <div className="media-body">
+                <h5 className="mt-0 mb-1">Boneco Minios Dave</h5>
+                <p>Boneco Dave do filme minios.</p>
+                <button type="button" className="btn btn-outline-dark">Reservar</button>
+              </div>
+            </li>
+            <li className="media pt-3">
+              <img src="https://s3.us-east-2.amazonaws.com/minios-imagens/minio-3.png" alt="" className="mr-3 img-pag-pricipal"/>
+              <div className="media-body">
+                <h5 className="mt-0 mb-1">Boneco Minios Stuart</h5>
+                <p>Boneco Stuart do filme minios.</p>
+                <button type="button" className="btn btn-outline-dark">Reservar</button>
+              </div>
+            </li>
+             <li className="media pt-3">
+              <img src="https://s3.us-east-2.amazonaws.com/minios-imagens/minio-4.png" alt="" className="mr-3 img-pag-pricipal"/>
+              <div className="media-body">
+                <h5 className="mt-0 mb-1">Boneco Minios Jerry</h5>
+                <p>Boneco Jerry do filme minios.</p>
+                <button type="button" className="btn btn-outline-dark">Reservar</button>
+              </div>
+            </li>
+            <li className="media pt-3">
+              <img src="https://s3.us-east-2.amazonaws.com/minios-imagens/minio-5.png" alt="" className="mr-3 img-pag-pricipal"/>
+              <div className="media-body">
+                <h5 className="mt-0 mb-1">Boneco Minios Bombeiro</h5>
+                <p>Boneco Bombeiro do filme minios.</p>
+                <button type="button" className="btn btn-outline-dark">Reservar</button>
+              </div>
+            </li>
+          </ul>
+        </div>
+        
+        
+        
+        <footer className="container col-12 pt-3 footer bg-dark text-light text-center">
+         
+        </footer>
       </div>
     );
   }
